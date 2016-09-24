fa54200b3886c52a44bea4ceb8e881d1edda2648
j49u4r <j49u4r@j49u4r.interware.com.mx>
2016-09-23 21:56:45 -0500

    Configuraciones de log, arranque por script

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Xmx96m -Xss256k -Dfile.encoding=UTF-8 -XX:MaxMetaspaceSize=48m -Dswarm.http.port=$PORT -jar build/libs/sample-1.0.war
--- src/main/java/com/j49u4r/rest/AppStart.groovy
+++ src/main/java/com/j49u4r/rest/AppStart.groovy
@@ -6,28 +6,39 @@ import org.wildfly.swarm.container.Container
 import org.wildfly.swarm.jaxrs.JAXRSArchive
 import org.wildfly.swarm.swagger.SwaggerArchive
 
+import java.util.logging.Logger
+
 /**
  * Created by j49u4r on 9/23/16.
  */
 class AppStart {
 
+    private static Logger logger;
+
     public static void main(String[] args) {
+
+        final Container container = new Container();
 
         System.setProperty("remote.maven.repo", "https://repository.jboss.org/nexus/content/groups/m2-proxy/,https://repository.jboss.org/nexus/content/groups/public/")
         System.setProperty("maven.download.message", "true")
 
-        final Container container = new Container();
 
         final JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
         deployment.addResource(JaxRsEntryPoint.class)
         deployment.addResource(CompanyCtrl.class)
         deployment.addResource(CorsFilter.class)
+        deployment.addAllDependencies()
 
         final SwaggerArchive archive = deployment.as(SwaggerArchive.class)
         archive.setResourcePackages("com.j49u4r.rest")
         archive.setTitle("Swagger Demo")
 
         container.start()
+
+        logger  = Logger.getLogger(AppStart.class.getName())
+        logger.info(deployment.toString(true))
+
         container.deploy()
+
     }
 }
